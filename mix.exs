3dda79836262cf8fcabaa3d2d352dd6f602e96b4
Brian Cardarella <brian@dockyard.com>
2016-02-09 20:26:54 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mail.Mixfile do
 
   def project do
     [app: :mail,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
