71754c551083afe334b5340dfd04550ae113ad94
Brandon Fish <brandon.j.fish@gmail.com>
2016-02-20 00:07:21 -0600

    Update Base64 encoder to encode and parse with newlines every 76 characters

---
+++ test/mail/encoders/base64_test.exs
@@ -0,0 +1,18 @@
+defmodule Mail.Encoders.Base64Test do
+  use ExUnit.Case
+
+  test "parses data with line feeds" do
+    base64_sample = "SGVsbG8gd29ybGQhIEhlbGxvIHdvcmxkISBIZWxsbyB3b3JsZCEgSGVsbG8g\r\nd29ybGQhIEhlbGxvIHdvcmxkISBIZWxsbyB3b3JsZCE=\r\n"
+    decoded = Mail.Encoders.Base64.decode(base64_sample)
+
+    assert decoded == "Hello world! Hello world! Hello world! Hello world! Hello world! Hello world!"
+  end
+
+
+  test "encodes data with line feeds" do
+    message = "Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world!"
+    encoded = Mail.Encoders.Base64.encode(message)
+
+    assert encoded == "SGVsbG8gd29ybGQhIEhlbGxvIHdvcmxkISBIZWxsbyB3b3JsZCEgSGVsbG8gd29ybGQhIEhlbGxv\r\nIHdvcmxkISBIZWxsbyB3b3JsZCEgSGVsbG8gd29ybGQhIEhlbGxvIHdvcmxkISBIZWxsbyB3b3Js\r\nZCE=\r\n"
+  end
+end
