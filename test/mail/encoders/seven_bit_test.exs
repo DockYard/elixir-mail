59e217d3fc86609790c59f81505a0eb9f6119e17
Ron Green <rgreenjr@gmail.com>
2016-03-04 16:33:56 -0600

    Initial commit of 7-bit encoder

---
+++ test/mail/encoders/seven_bit_test.exs
@@ -0,0 +1,36 @@
+defmodule Mail.Encoders.SevenBitTest do
+  use ExUnit.Case
+
+  test "encode handles empty strings" do
+    assert Mail.Encoders.SevenBit.encode("") == ""
+  end
+
+  test "encode wraps lines longer than 1000 characters" do
+    message = String.duplicate("-", 2000)
+    encoding = Mail.Encoders.SevenBit.encode(message)
+    assert binary_part(encoding, 998, 2) == "\r\n"
+    assert binary_part(encoding, 1998, 2) == "\r\n"
+    assert byte_size(encoding) == 2004
+  end
+
+  test "encode raises if any character isn't 7-bit ASCII" do
+    assert_raise ArgumentError, fn ->
+      Mail.Encoders.SevenBit.encode("hełło")
+    end
+  end
+
+  test "encode raises if any character is <NUL>" do
+    assert_raise ArgumentError, fn ->
+      Mail.Encoders.SevenBit.encode("\0")
+    end
+  end
+
+  test "decode handles empty strings" do
+    assert Mail.Encoders.SevenBit.decode("") == ""
+  end
+
+  test "decode removes <CR><LF> pairs" do
+    message = "This is a \r\ntest\r\n"
+    assert Mail.Encoders.SevenBit.decode(message) == "This is a test"
+  end
+end
