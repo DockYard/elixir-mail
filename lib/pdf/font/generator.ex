6b18c11baf39d222fd9756136bd7acdbd0b1528b
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-10-18 11:02:23 +0200

    Add Pdf.Font.Generator to generate font modules
    Modules are generated from Adobe AFM files

---
+++ lib/pdf/font/generator.ex
@@ -0,0 +1,104 @@
+defmodule Pdf.Font.Generator do
+  @moduledoc false
+  alias Pdf.Font.Metrics
+
+  font_metrics =
+    Path.join(__DIR__, "../../fonts/*.afm")
+    |> Path.wildcard
+    |> Enum.map(fn(afm_file) ->
+    # IO.puts afm_file
+
+    metrics =
+      File.stream!(afm_file)
+      |> Enum.reduce(%Metrics{}, fn(line, metrics) ->
+        Metrics.process_line(String.replace_suffix(line, "\n", ""), metrics)
+      end)
+    metrics = %{metrics | widths: Enum.reverse(metrics.widths)}
+    %{metrics | last_char: metrics.first_char + length(metrics.widths)}
+  end)
+
+  font_metrics
+  |> Enum.each(fn(metrics) ->
+    font_module = String.to_atom("Elixir.Pdf.Font.#{String.replace(metrics.name, "-", "")}")
+    defmodule font_module do
+      @doc "The name of the font"
+      def name, do: unquote(metrics.name)
+      @doc "The full name of the font"
+      def full_name, do: unquote(metrics.full_name)
+      @doc "The font family of the font"
+      def family, do: unquote(metrics.family)
+      @doc "The font weight"
+      def weight, do: unquote(metrics.weight)
+      @doc "The font italic angle"
+      def italic_angle, do: unquote(metrics.italic_angle)
+      @doc "The font encoding"
+      def encoding, do: unquote(metrics.encoding)
+      @doc "The first character defined in `widths/0`"
+      def first_char, do: unquote(metrics.first_char)
+      @doc "The last character defined in `widths/0`"
+      def last_char, do: unquote(metrics.last_char)
+      @doc "The font ascender"
+      def ascender, do: unquote(metrics.ascender)
+      @doc "The font descender"
+      def descender, do: unquote(metrics.descender)
+      @doc "The font cap height"
+      def cap_height, do: unquote(metrics.cap_height)
+      @doc "The font x-height"
+      def x_height, do: unquote(metrics.x_height)
+      @doc """
+      Returns the character widths of characters beginning from `first_char/0`
+      """
+      def widths, do: unquote(metrics.widths)
+
+      @doc """
+      Returns the width of the specific character
+
+      Examples:
+
+          iex> #{inspect(__MODULE__)}.width("A")
+          123
+      """
+      def width(utf8_char)
+      metrics.widths
+      |> Enum.with_index(metrics.first_char)
+      |> Enum.each(fn({width, char_code}) ->
+        # def width(unquote(char_code)), do: unquote(width)
+        def width(<< unquote(char_code) :: utf8 >>), do: unquote(width)
+      end)
+
+      @doc ~S"""
+      Returns the width of the string in font units (1/1000 of font scale factor)
+      """
+      def text_width(string) do
+        string
+        |> String.codepoints
+        |> Enum.reduce(0, &(&2 + width(&1)))
+      end
+
+      @doc ~S"""
+      Returns the width of a string in points (72 points = 1 inch)
+      """
+      def text_width(string, font_size) do
+        width = text_width(string)
+        width * font_size / 1000
+      end
+    end
+  end)
+
+  defmodule Elixir.Pdf.Font do
+    @doc ~S"""
+    Returns the font module for the named font
+
+    # Example:
+
+        iex> Pdf.Font.lookup("Helvetica-BoldOblique")
+        Pdf.Font.HelveticaBoldOblique
+    """
+    def lookup(name)
+    font_metrics
+    |> Enum.each(fn(metrics) ->
+      font_module = String.to_atom("Elixir.Pdf.Font.#{String.replace(metrics.name, "-", "")}")
+      def lookup(unquote(metrics.name)), do: unquote(font_module)
+    end)
+  end
+end
