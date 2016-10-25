1d6ae338a97321f2b6567bee2ff84733cbdf5340
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-10-25 12:28:50 +0200

    Add assert_unchanged function to better handling refactoring

--- test/pdf_test.exs
+++ test/pdf_test.exs
@@ -6,27 +6,29 @@ defmodule PdfTest do
   end
 
   test "new/1" do
-    {:ok, pdf} = Pdf.new(size: :a4)
-    file_path = Path.join(__DIR__, "../tmp/test.pdf")
-    pdf
-    |> Pdf.set_author("Test Author")
-    |> Pdf.set_creator("Test Creator")
-    |> Pdf.set_keywords("word word word")
-    |> Pdf.set_producer("Test producer")
-    |> Pdf.set_subject("Test Subject")
-    |> Pdf.set_title("Test Document")
-    |> Pdf.set_font("Helvetica", 12)
-    |> Pdf.text_at({10, 400}, "Hello World")
-    |> Pdf.text_lines({10, 300}, [
-      "First line",
-      "Second line",
-      "Third line"
-      ])
-    |> Pdf.add_image({25, 50}, fixture("rgb.jpg"))
-    |> Pdf.add_image({175, 50}, fixture("cmyk.jpg"))
-    |> Pdf.add_image({325, 50}, fixture("grayscale.jpg"))
-    |> Pdf.write_to(file_path)
-    |> Pdf.delete
+    file_path = output("qtest.pdf")
+    assert_unchanged(file_path, fn ->
+      {:ok, pdf} = Pdf.new(size: :a4)
+      pdf
+      |> Pdf.set_author("Test Author")
+      |> Pdf.set_creator("Test Creator")
+      |> Pdf.set_keywords("word word word")
+      |> Pdf.set_producer("Test producer")
+      |> Pdf.set_subject("Test Subject")
+      |> Pdf.set_title("Test Document")
+      |> Pdf.set_font("Helvetica", 12)
+      |> Pdf.text_at({10, 400}, "Hello World")
+      |> Pdf.text_lines({10, 300}, [
+        "First line",
+        "Second line",
+        "Third line"
+        ])
+      |> Pdf.add_image({25, 50}, fixture("rgb.jpg"))
+      |> Pdf.add_image({175, 50}, fixture("cmyk.jpg"))
+      |> Pdf.add_image({325, 50}, fixture("grayscale.jpg"))
+      |> Pdf.write_to(file_path)
+      |> Pdf.delete
+    end)
 
     System.cmd("open", ["-g", file_path])
   end
--- test/support/pdf_case.ex
+++ test/support/pdf_case.ex
@@ -9,4 +9,48 @@ defmodule Pdf.Case do
 
   def fixture(path),
     do: __DIR__ |> Path.join("fixtures") |> Path.join(path)
+
+  def output(path),
+    do: __DIR__ |> Path.join("../output") |> Path.join(path)
+
+  def assert_unchanged(file_path, func) do
+    path = Path.expand(file_path)
+    if File.exists?(path) do
+      changed_path = build_changed_path(path)
+      try do
+        original = File.read!(path)
+        func.()
+        changed = File.read!(path)
+
+        if original != changed do
+          failure_message = "PDF output has changed"
+          File.write!(path, original) # Re-save original file
+          diff = System.find_executable("diff")
+          if diff do
+            File.write!(changed_path, changed) # Save copy of changes
+            case System.cmd(diff, ["-au", path, changed_path]) do # Do diff on changes
+              {diff_results, 1} ->
+                assert false, failure_message <> "\nDiff:\n" <> diff_results
+              _ ->
+                assert false, failure_message
+            end
+          else
+            assert false, failure_message
+          end
+        end
+      after
+        if File.exists?(changed_path), do: File.rm(changed_path) # Delete copy of changes
+      end
+    else
+      func.()
+    end
+  end
+
+  defp build_changed_path(path) do
+    [basename, ext] =
+      path
+      |> Path.basename
+      |> String.split(".")
+    Path.expand(Path.join(path, "../#{basename}-changed.#{ext}"))
+  end
 end
