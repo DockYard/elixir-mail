a565b7d661bc15baf77541a5c7f68dabfdefdbd6
Corey Powell <mistdragon100@gmail.com>
2017-02-22 21:28:29 -0500

    Rewrote the Encoder module
    * Do not convert incoming encoding strings to atoms (memory leak if you
    get different encodings each time in different casings)
    * Normalize the encoding (some servers, clients will send BASE64 or
    Base64 instead of base64)
    * Support 7bit, 8bit, and quoted-printable (especially quoted-printable)

---
+++ test/mail/encoder_test.exs
@@ -0,0 +1,38 @@
+defmodule Mail.EncoderTest do
+  use ExUnit.Case, async: true
+  alias Mail.Encoder
+
+  describe "encode/decode" do
+    test "encodes a binary in 7bit" do
+      # odd casing
+      # nothing to do here really...
+      assert "Hello,\nWorld" == Encoder.encode("Hello,\nWorld", "7BIT")
+      assert "Hello,\nWorld" == Encoder.decode("Hello,\nWorld", "7Bit")
+    end
+
+    test "encodes a binary in 8bit" do
+      # odd casing
+      # nothing to do here really...
+      assert "Hello,\nWorld" == Encoder.encode("Hello,\nWorld", "8BIT")
+      assert "Hello,\nWorld" == Encoder.decode("Hello,\nWorld", "8Bit")
+    end
+
+    test "encodes a binary as base64" do
+      # with odd casings
+      assert "SGVsbG8sIFdvcmxk\r\n" == Encoder.encode("Hello, World", "BASE64")
+      assert "Hello, World" == Encoder.decode("SGVsbG8sIFdvcmxk\r\n", "Base64")
+    end
+
+    test "encodes a binary in quoted-printable" do
+      # odd casing
+      assert "Hello,=0AWorld" == Encoder.encode("Hello,\nWorld", "Quoted-Printable")
+      assert "Hello,\nWorld" == Encoder.decode("Hello,=0AWorld", "QUOTED-PRINTABLE")
+    end
+
+    test "... everything else" do
+      assert "Hello,\nWorld" == Encoder.encode("Hello,\nWorld", "ASCII")
+      assert "Hello,\nWorld" == Encoder.decode("Hello,\nWorld", "UTF-8")
+      assert "Hello,\nWorld" == Encoder.decode("Hello,\nWorld", "binary")
+    end
+  end
+end
