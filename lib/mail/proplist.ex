120f950c41910078a67d4e5e463b90fea165a4fe
Corey Powell <mistdragon100@gmail.com>
2017-02-28 11:56:16 -0500

    Added proplist documentation
    Or at least really basic kinds

--- lib/mail/proplist.ex
+++ lib/mail/proplist.ex
@@ -1,6 +1,17 @@
 defmodule Mail.Proplist do
+  @moduledoc """
+  A hybrid of erlang's proplists and lists keystores
+  """
+
   @type t :: [{term, term} | term]
 
+  @doc """
+  Retrieves all keys from the key value pairs present in the list,
+  unlike :proplists.get_keys which will return non-kv pairs as keys
+
+  Args:
+  * `list` - a list to retrieve all the keys from
+  """
   @spec keys(list :: __MODULE__.t) :: [term]
   def keys(list) do
     Enum.reduce(list, [], fn
@@ -16,11 +27,27 @@ defmodule Mail.Proplist do
     |> Enum.reverse()
   end
 
+  @doc """
+  Stores a key-value pair in the list, will replace an existing pair with the
+  same key.
+
+  Args:
+  * `list` - the list to store in
+  * `key` - the key of the pair
+  * `value` - the value of the pair
+  """
   @spec put(list :: __MODULE__.t, key :: term, value :: term) :: __MODULE__.t
   def put(list, key, value) do
     :lists.keystore(key, 1, list, {key, value})
   end
 
+  @doc """
+  Retrieves a value from the list
+
+  Args:
+  * `list` - the list to look in
+  * `key` - the key of the pair to retrieve it's value
+  """
   @spec get(list :: __MODULE__.t, key :: term) :: __MODULE__.t
   def get(list, key) do
     case :proplists.get_value(key, list) do
@@ -29,6 +56,13 @@ defmodule Mail.Proplist do
     end
   end
 
+  @doc """
+  Concatentates the given lists.
+
+  Args:
+  * `a` - base list to merge unto
+  * `b` - list to merge with
+  """
   @spec merge(a :: __MODULE__.t, b :: __MODULE__.t) :: __MODULE__.t
   def merge(a, b) do
     Enum.reduce(a ++ b, [], fn
@@ -47,11 +81,26 @@ defmodule Mail.Proplist do
     |> Enum.reverse()
   end
 
+  @doc """
+  Removes a key-value pair by the given key and returns the remaining list
+
+  Args:
+  * `list` - the list to remove the pair from
+  * `key` - the key to remove
+  """
   @spec delete(list :: __MODULE__.t, key :: term) :: __MODULE__.t
   def delete(list, key) do
     :proplists.delete(key, list)
   end
 
+  @doc """
+  Filters the proplist, i.e. returns only those elements
+  for which `fun` returns a truthy value.
+
+  Args:
+  * `list` - the list to filter
+  * `func` - the function to execute
+  """
   @spec filter(list :: __MODULE__.t, func :: any) :: __MODULE__.t
   def filter(list, func) do
     Enum.filter(list, fn
@@ -60,11 +109,25 @@ defmodule Mail.Proplist do
     end)
   end
 
+  @doc """
+  Drops the specified keys from the list, returning the remaining.
+
+  Args:
+  * `list` - the list
+  * `keys` - the keys to remove
+  """
   @spec drop(list :: __MODULE__.t, keys :: list) :: __MODULE__.t
   def drop(list, keys) do
     filter(list, fn {key, value} -> !Enum.member?(keys, key) end)
   end
 
+  @doc """
+  Takes the specified keys from the list, returning the remaining.
+
+  Args:
+  * `list` - the list
+  * `keys` - the keys to keep
+  """
   @spec take(list :: __MODULE__.t, keys :: list) :: __MODULE__.t
   def take(list, keys) do
     filter(list, fn {key, value} -> Enum.member?(keys, key) end)
