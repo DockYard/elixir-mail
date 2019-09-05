4a41cc6213dbc14fd9efbca386b3aea5ecb8e25f
Herman verschooten <Herman@verschooten.net>
2019-09-05 15:42:10 +0200

    Test Object.size

--- test/pdf/object_test.exs
+++ test/pdf/object_test.exs
@@ -2,6 +2,9 @@ defmodule Pdf.ObjectTest do
   use ExUnit.Case, async: true
 
   alias Pdf.Object
+  alias Pdf.Dictionary
+  alias Pdf.Utils
+  alias Pdf.Array
 
   test "new/1" do
     object = Object.new(1)
@@ -35,5 +38,27 @@ defmodule Pdf.ObjectTest do
     object = Object.new(13)
 
     assert Object.reference(object) == "13 0 R"
+  end
+
+  test "size" do
+    dict =
+      Dictionary.new()
+      |> Dictionary.put("Author", Utils.n("Test Author"))
+      |> Dictionary.put("Creator", Utils.n("Test Creator"))
+      |> Dictionary.put("Keywords", Utils.n("word word word"))
+      |> Dictionary.put("Producer", Utils.n("Test producer"))
+      |> Dictionary.put("Subject", Utils.n("Test Subject"))
+      |> Dictionary.put("Title", Utils.n("Test Document"))
+      |> Dictionary.put("Title", Utils.n("Test Document"))
+      |> Dictionary.put("Title", Utils.n("Test Document"))
+      |> IO.inspect()
+
+    object = Object.new(1, dict)
+
+    export =
+      Pdf.Export.to_iolist(object)
+      |> Enum.join()
+
+    assert Object.size(object) == byte_size(export)
   end
 end
