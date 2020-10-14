97ecd591654c19852950fc72f9910e2df2f089e1
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-10-14 09:14:39 -0400

    Include :crypto as an application dependency

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Mail.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:crypto, :logger]]
   end
 
   def package do
