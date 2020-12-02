cd1d7ccefff81086ca6cf6b633ae39be9d4989ca
Andrew Timberlake <andrew@andrewtimberlake.com>
2020-12-02 09:37:35 +0200

    Clean up compiler warnings

--- lib/pdf/object_collection.ex
+++ lib/pdf/object_collection.ex
@@ -15,30 +15,30 @@ defmodule Pdf.ObjectCollection do
 
   def init(_), do: {:ok, %State{}}
 
-  defcall create_object(object, _from, %State{size: size, objects: objects} = state) do
-    new_size = size + 1
+  defcall create_object(object, _from, state) do
+    new_size = state.size + 1
     key = {:object, new_size, 0}
-    {:reply, key, %{state | size: new_size, objects: Map.put(objects, key, object)}}
+    {:reply, key, %{state | size: new_size, objects: Map.put(state.objects, key, object)}}
   end
 
-  defcall get_object(key, _from, %State{objects: objects} = state) do
-    object = Map.get(objects, key)
+  defcall get_object(key, _from, state) do
+    object = Map.get(state.objects, key)
     {:reply, object, state}
   end
 
-  defcall update_object(key, value, _from, %State{objects: objects} = state) do
-    {:reply, :ok, %{state | objects: Map.put(objects, key, value)}}
+  defcall update_object(key, value, _from, state) do
+    {:reply, :ok, %{state | objects: Map.put(state.objects, key, value)}}
   end
 
-  defcall call(object_key, method, args, _from, %State{objects: objects} = state) do
-    object = Map.get(objects, object_key)
+  defcall call(object_key, method, args, _from, state) do
+    object = Map.get(state.objects, object_key)
     result = Kernel.apply(object.__struct__, method, [object | args])
-    {:reply, object_key, %{state | objects: Map.put(objects, object_key, result)}}
+    {:reply, object_key, %{state | objects: Map.put(state.objects, object_key, result)}}
   end
 
-  defcall all(_from, %State{objects: objects} = state) do
+  defcall all(_from, state) do
     result =
-      objects
+      state.objects
       |> Enum.map(fn {{:object, number, _generation}, object} ->
         Object.new(number, object)
       end)
