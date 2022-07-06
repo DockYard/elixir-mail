8917fe9a9fd5c3385fd6cb4bc3b9583d1fe015e8
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-07-06 12:32:46 +0200

    refactor: decoder proxy state
    I've realized that it might not be safe to use a global state to store
    the `impl` module since test are async.

--- test/support/rfc2822_body_decoder_proxy.ex
+++ test/support/rfc2822_body_decoder_proxy.ex
@@ -1,16 +1,59 @@
 defmodule Mail.RFC2822BodyDecoderProxy do
-  use Agent
+  use GenServer
 
   @behaviour Mail.Parsers.RFC2822.BodyDecoder
 
   @strict_impl Mail.Parsers.RFC2822.BodyDecoder.Strict
   @permissive_impl Mail.Parsers.RFC2822.BodyDecoder.Permissive
 
-  def start_link(impl \\ @strict_impl), do: Agent.start_link(fn -> impl end, name: __MODULE__)
+  def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, opts, name: __MODULE__)
 
-  def strict_impl, do: Agent.update(__MODULE__, fn _ -> @strict_impl end)
-  def permissive_impl, do: Agent.update(__MODULE__, fn _ -> @permissive_impl end)
+  def strict_impl, do: GenServer.call(__MODULE__, {:set_impl, @strict_impl})
 
-  @impl true
-  def decode(body, message), do: Agent.get(__MODULE__, & &1).decode(body, message)
+  def permissive_impl, do: GenServer.call(__MODULE__, {:set_impl, @permissive_impl})
+
+  @impl Mail.Parsers.RFC2822.BodyDecoder
+  def decode(body, message) do
+    case GenServer.call(__MODULE__, {:decode, body, message}) do
+      {:ok, decoded} ->
+        decoded
+
+      {:error, :pid_not_found} ->
+        raise RuntimeError, "You should set an impl module for decoder proxy"
+    end
+  end
+
+  @impl GenServer
+  def init(opts), do: {:ok, {Keyword.get(opts, :default_impl), %{}}}
+
+  @impl GenServer
+  def handle_call({:set_impl, impl}, {test_pid, _tag}, {default_impl, pid_map}) do
+    Process.monitor(test_pid)
+
+    {:reply, :ok, {default_impl, Map.put(pid_map, test_pid, impl)}}
+  end
+
+  def handle_call({:decode, body, message}, {test_pid, _tag}, {nil, pid_map} = state) do
+    case Map.get(pid_map, test_pid) do
+      nil ->
+        {:reply, {:error, :pid_not_found}, state}
+
+      impl ->
+        {:reply, {:ok, impl.decode(body, message)}, state}
+    end
+  end
+
+  def handle_call({:decode, body, message}, {test_pid, _tag}, {default_impl, pid_map} = state) do
+    case Map.get(pid_map, test_pid) do
+      nil ->
+        {:reply, {:ok, default_impl.decode(body, message)}, state}
+
+      impl ->
+        {:reply, {:ok, impl.decode(body, message)}, state}
+    end
+  end
+
+  @impl GenServer
+  def handle_info({:DOWN, _ref, :process, test_pid, _reason}, {default_impl, pid_map}),
+    do: {:noreply, {default_impl, Map.delete(pid_map, test_pid)}}
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
-Mail.RFC2822BodyDecoderProxy.start_link()
+Mail.RFC2822BodyDecoderProxy.start_link(default_impl: Mail.Parsers.RFC2822.BodyDecoder.Strict)
 
 ExUnit.start()
